383502028ec0e776076a336e6edbfc98821c379b
Ofer Zelig <ofer.zelig@gmail.com>
2016-12-13 01:03:31 +1100

    Fixed the directive such that it will now properly delete one of the pair (red or green) before adding

--- red-green.directive.ts
+++ red-green.directive.ts
@@ -6,7 +6,16 @@ import { Directive, ElementRef, Input, Renderer, HostListener } from '@angular/c
 export class RedGreenDirective {
 
     @Input('red-green') set redGreen(value: boolean) {
-        this.renderer.setElementClass(this.el.nativeElement, value ? 'green' : 'red', true);
+
+      // First explicitly remove the other color, if it's already set. Otherwise we'll be adding to an existing classList (though we do want adding in general, for other class names used)
+      var classList = this.el.nativeElement.classList;
+      if (value && classList.contains('red'))
+          classList.remove('red');
+      if (!value && classList.contains('green'))
+          classList.remove('green');
+
+      // Now add the required color
+      this.renderer.setElementClass(this.el.nativeElement, value ? 'green' : 'red', true);
     };
 
     constructor(private el: ElementRef, private renderer: Renderer) { }
